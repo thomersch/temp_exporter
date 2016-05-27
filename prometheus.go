06d860f65e5c551004c8c9fd89658be76cd2793e
Thomas Skowron <th@skowron.biz>
2016-05-27 18:02:09 +0200

    exit on error to not write zero values

--- prometheus.go
+++ prometheus.go
@@ -41,6 +41,7 @@ func (e *Exporter) Collect(ch chan<- prometheus.Metric) {
 		vals, err := readSensor(target)
 		if err != nil {
 			log.Printf("could not read from sensor: %s", err)
+			return
 		}
 
 		temperature.WithLabelValues(target).Set(vals.temperature)
